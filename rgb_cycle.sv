057db60d7cc8e704a808f4448762cf128c314b13
KiwiAviation <jacobkeller737@gmail.com>
2026-09-21 21:56:10 -0400

    Implement RGB cycle

--- rgb_cycle.sv
+++ rgb_cycle.sv
@@ -1,25 +1,59 @@
 // RGB Cycle
 
-module top(
+module top #(
+    parameter CLK_FREQ = 12000000,
+    parameter TICKS_PER_CYCLE = CLK_FREQ / 6
+)(
     input logic     clk, 
-    output logic    LED
+    output logic    RGB_R,
+    output logic    RGB_G,
+    output logic    RGB_B
 );
-
-    // CLK frequency is 12MHz, so 6,000,000 cycles is 0.5s
-    parameter BLINK_INTERVAL = 6000000;
-    logic [$clog2(BLINK_INTERVAL) - 1:0] count = 0;
+    
+    logic [$clog2(CLK_FREQ) - 1:0] count = 1'b0;
 
     initial begin
-        LED = 1'b0;
+        RGB_R = 1'b0;
+        RGB_G = 1'b1;
+        RGB_B = 1'b1;
     end
 
     always_ff @(posedge clk) begin
-        if (count == BLINK_INTERVAL - 1) begin
-            count <= 0;
-            LED <= ~LED;
+        if (count >= CLK_FREQ - 1) begin
+            count <= 1'b0;
+        end else begin
+            count <= count + 1'b1;
         end
-        else begin
-            count <= count + 1;
+
+        if (count <= (1 * TICKS_PER_CYCLE)) begin
+            RGB_R <= 1'b0;
+            RGB_G <= 1'b1;
+            RGB_B <= 1'b1;
+        end
+        else if (count <= (2 * TICKS_PER_CYCLE)) begin
+            RGB_R <= 1'b0;
+            RGB_G <= 1'b0;
+            RGB_B <= 1'b1;
+        end
+        else if (count <= (3 * TICKS_PER_CYCLE)) begin
+            RGB_R <= 1'b1;
+            RGB_G <= 1'b0;
+            RGB_B <= 1'b1;
+        end
+        else if (count <= (4 * TICKS_PER_CYCLE)) begin
+            RGB_R <= 1'b1;
+            RGB_G <= 1'b0;
+            RGB_B <= 1'b0;
+        end
+        else if (count <= (5 * TICKS_PER_CYCLE)) begin
+            RGB_R <= 1'b1;
+            RGB_G <= 1'b1;
+            RGB_B <= 1'b0;
+        end
+        else if (count <= (6 * TICKS_PER_CYCLE)) begin
+            RGB_R <= 1'b0;
+            RGB_G <= 1'b1;
+            RGB_B <= 1'b0;
         end
     end
 
